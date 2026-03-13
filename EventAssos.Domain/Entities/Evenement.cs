369bbdad9726854c854091898fb2bc673daa031e
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-13 20:00:03 +0100

    entité Evenement.cs

---
+++ EventAssos.Domain/Entities/Evenement.cs
@@ -0,0 +1,20 @@
+﻿using EventAssos.Domain.Enums;
+
+namespace EventAssos.Domain.Entities;
+
+public class Evenement
+{
+  public Guid id { get; set; }
+  public required string Description { get; set; }
+  public string Lieu  { get; set; }
+  public DateTime DateDebut { get; set; }
+  public DateTime DateFin { get; set; }
+  public int NbMin { get; set; }
+  public int NbMax { get; set; }
+  public StatutEvenement StatutEvenement { get; set; }
+  public DateTime DateLimiteInscription { get; set; }
+  public DateTime DateDeCreation { get; set; }
+  public DateTime DateMaj { get; set; }
+}
+
+
