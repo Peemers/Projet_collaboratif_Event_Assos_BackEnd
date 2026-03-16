bae6a20f1574a12d597ede2cb4e3968a97f2e9db
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-16 09:47:53 +0100

    Install des NuGet, et création du DbContext

---
+++ EventAssos.Infrastructure/DataBase/Context/EventAssosDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EventAssos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventAssos.Infrastructure.DataBase.Context;
+
+public class EventAssosDbContext : DbContext
+{
+  public EventAssosDbContext(DbContextOptions<EventAssosDbContext> options) : base(options)
+  {
+  }
+
+  public DbSet<Categorie> Categories { get; set; } = null!;
+  public DbSet<Evenement> Evenements { get; set; } = null!;
+  public DbSet<Inscription> Inscriptions { get; set; } = null!;
+  public DbSet<Membre> Membres { get; set; } = null!;
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(EventAssosDbContext).Assembly);
+  }
+
+}
