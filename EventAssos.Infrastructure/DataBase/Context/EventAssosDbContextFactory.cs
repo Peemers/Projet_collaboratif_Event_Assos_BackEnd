2d04e3b38f83ed118c668c3ae3d6814e7c69ed3d
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-16 16:24:31 +0100

    ajout EventAssosDbContextFactory.cs

---
+++ EventAssos.Infrastructure/DataBase/Context/EventAssosDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
+
+namespace EventAssos.Infrastructure.DataBase.Context;
+
+public class EventAssosDbContextFactory : IDesignTimeDbContextFactory<EventAssosDbContext>
+{
+  public EventAssosDbContext CreateDbContext(string[] args)
+  {
+    string path = Path.Combine(Directory.GetCurrentDirectory(), "..", "EventAssos.Api");
+
+    IConfigurationRoot configuration = new ConfigurationBuilder()
+      .SetBasePath(path)
+      .AddJsonFile("appsettings.Development.json")
+      .Build();
+
+    var connectionString = configuration.GetConnectionString("Default");
+
+    var optionBuilder = new DbContextOptionsBuilder<EventAssosDbContext>();
+    optionBuilder.UseSqlServer(connectionString);
+    return new EventAssosDbContext(optionBuilder.Options);
+  }
+}
