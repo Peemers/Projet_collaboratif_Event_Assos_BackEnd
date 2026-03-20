a725b930e4a29fbf238cdb03cb99c10ad1ca614a
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-20 15:22:59 +0100

    création IMembreRepository.cs

--- EventAssos.Core/Interfaces/Repositories/IBaseRepository.cs
+++ EventAssos.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -3,7 +3,7 @@
 public interface IBaseRepository<T, TId> where T : class
 {
   Task<IEnumerable<T>> GetAllAsync();
-  Task<T> GetByIdAsync(TId id);
+  Task<T?> GetByIdAsync(TId id);
   Task<T> AddAsync(T entity);
   Task<T> UpdateAsync(T entity);
   Task<T> DeleteAsync(TId id);
