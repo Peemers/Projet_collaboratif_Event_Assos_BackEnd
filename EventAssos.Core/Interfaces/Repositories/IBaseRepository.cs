25aa7b83c1df3864ca2ef6724bff40ea931d2417
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-20 15:13:44 +0100

    IBaseRepository.cs

---
+++ EventAssos.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace EventAssos.Core.Interfaces.Repositories;
+
+public interface IBaseRepository<T, TId> where T : class
+{
+  Task<IEnumerable<T>> GetAllAsync();
+  Task<T> GetByIdAsync(TId id);
+  Task<T> AddAsync(T entity);
+  Task<T> UpdateAsync(T entity);
+  Task<T> DeleteAsync(TId id);
+}
