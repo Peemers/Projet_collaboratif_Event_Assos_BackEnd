d382dd064c55d2b99ec9fcd000bc5d27cd6c1cc2
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-25 11:54:01 +0100

    création EvenementRequestDto.cs, EvenementMapper.cs, EvenementShortResponseDto.cs

--- EventAssos.Core/DTOs/Request/EvenementRequestDtos/EvenementRequestDto.cs
+++ EventAssos.Core/DTOs/Request/EvenementRequestDtos/EvenementRequestDto.cs
@@ -9,7 +9,7 @@ public class EvenementRequestDto
   public required string Nom { get; set; }
   
   [Required(ErrorMessage = "La déscription est obligatoire")]
-  [MaxLength(512)]
+  [MaxLength(150)]
   public required string Description { get; set; }
   
   [MaxLength(256)]
@@ -27,7 +27,7 @@ public class EvenementRequestDto
   [Range(1, 200, ErrorMessage = "Le nombre de participants doit être entre 1 et 200")]
   public int NbMax { get; set; }
   
-  public required bool ListeAttenteActive { get; set; }
+  public bool ListeAttenteActive { get; set; }
   
   [Required(ErrorMessage = "La date d'inscription est obligatoire")]
   public DateTime DateInscription { get; set; }
@@ -35,5 +35,5 @@ public class EvenementRequestDto
   [Required(ErrorMessage = "La date de limite d'inscription est obligatoire")]
   public DateTime DateLimiteInscription { get; set; }
   
-  public List<int> CategorieIds { get; set; } = new();
+  public List<int> CategorieIds { get; set; } = new(); //liste des id de categories choisies
 }
---
+++ EventAssos.Core/Mappers/EvenementMapper.cs
@@ -0,0 +1,51 @@
+﻿using EventAssos.Core.DTOs.Request.EvenementRequestDtos;
+using EventAssos.Core.DTOs.Response.EvenementResponseDtos;
+using EventAssos.Domain.Entities;
+using EventAssos.Domain.Enums;
+
+namespace EventAssos.Core.Mappers;
+
+public static class EvenementMapper
+{
+  public static Evenement ToEntity(this EvenementRequestDto dto)
+  {
+    return new Evenement()
+    {
+      Id = Guid.NewGuid(),
+      Nom = dto.Nom,
+      Description = dto.Description,
+      Lieu = dto.Lieu,
+      DateDebut = dto.DateDebut.Date,
+      DateFin = dto.DateFin.Date,
+      NbMin = dto.NbMin,
+      NbMax = dto.NbMax,
+      ListeAttenteActive = dto.ListeAttenteActive,
+      DateLimiteInscription = dto.DateLimiteInscription,
+      StatutEvenement = StatutEvenement.EnAttente, //pour l'énoncé 
+      DateDeCreation = DateTime.UtcNow,
+      DateMaj = DateTime.UtcNow
+    };
+  }
+
+  public static EvenementShortResponseDto ToShortResponseDto(this Evenement evenement)
+  {
+    return new EvenementShortResponseDto()
+    {
+      Id = evenement.Id,
+      Nom = evenement.Nom,
+      Description = evenement.Description.Length > 150 //ternaire si evenement.... > 150, énoncé
+        ? evenement.Description.Substring(0, 150) /*[..150]*/ + "..."
+        : evenement.Description,
+      Lieu = evenement.Lieu,
+      DateDebut = evenement.DateDebut,
+      DateFin = evenement.DateFin,
+      NbInscrits = evenement.Inscriptions.Count(i => !i.EstEnAttente), //calcul de la prop, exclure ceux en liste d'attente
+      NbMin = evenement.NbMin,
+      NbMax = evenement.NbMax,
+      Categories = evenement.Categories.Select(c => c.Nom).ToList(),
+      Statut = evenement.StatutEvenement.ToString(), //tostring, la prop dans le dto est une string}
+      DateLimiteInscription = evenement.DateLimiteInscription,
+      ListeAttenteActive = evenement.ListeAttenteActive,
+    };
+  }
+}
