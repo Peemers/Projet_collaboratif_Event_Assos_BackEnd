e5f4dc7fc33f5913463ae75abb2d0a62f7c7ff3c
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-25 12:44:33 +0100

    création de ICategorieRepository.cs, CategorieRepository.cs (A terminer), injection dans Program.cs.

---
+++ EventAssos.Infrastructure/Repositories/CategorieRepository.cs
@@ -0,0 +1,10 @@
+﻿using EventAssos.Core.Interfaces.Repositories;
+using EventAssos.Domain.Entities;
+using EventAssos.Infrastructure.DataBase.Context;
+
+namespace EventAssos.Infrastructure.Repositories;
+
+public class CategorieRepository(EventAssosDbContext context) : BaseRepository<Categorie, int>(context), ICategorieRepository
+{
+  
+}
