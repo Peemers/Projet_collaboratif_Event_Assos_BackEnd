85234de6542b102041107b0b99d66e502d51931b
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-25 15:47:11 +0100

    création InscriptionRepository.cs et IInscriptionRepository.cs

---
+++ EventAssos.Infrastructure/Repositories/InscriptionRepository.cs
@@ -0,0 +1,17 @@
+﻿using EventAssos.Core.Interfaces.Repositories;
+using EventAssos.Domain.Entities;
+using EventAssos.Infrastructure.DataBase.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventAssos.Infrastructure.Repositories;
+
+public class InscriptionRepository(EventAssosDbContext context) : BaseRepository<Inscription, Guid>(context), IInscriptionRepository
+{
+  public async Task<Inscription?> GetInscriptionExisteAsync(Guid membreId, Guid evenementId)
+  {
+    Inscription? inscription = await context.Inscriptions
+      .FirstOrDefaultAsync(i => i.MembreId == membreId && i.EvenementId == evenementId);
+    
+    return inscription;
+  }
+}
