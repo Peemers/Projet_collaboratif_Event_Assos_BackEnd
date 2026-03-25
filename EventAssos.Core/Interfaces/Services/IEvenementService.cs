5f07a578384640102faaf61f49a3b9ec9cd136e8
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-25 13:27:38 +0100

    création IEvenementService.cs, EvenementService.cs (saloperie)

---
+++ EventAssos.Core/Interfaces/Services/IEvenementService.cs
@@ -0,0 +1,10 @@
+﻿using EventAssos.Core.DTOs.Request.EvenementRequestDtos;
+using EventAssos.Core.DTOs.Response.EvenementResponseDtos;
+
+namespace EventAssos.Core.Interfaces.Services;
+
+public interface IEvenementService
+{
+  Task<EvenementDetailsResponseDto> CreateAsync(EvenementRequestDto dto);
+  Task<IEnumerable<EvenementShortResponseDto>> GetLatestAsync();
+}
