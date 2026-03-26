dd997c18c0c0f881bbf0b8877559ab95d0098307
Math Peeters <pmathieu.peeters@gmail.com>
2026-03-26 16:49:23 +0100

    Correction de bug logger : AuthService.cs Création de tests unitaire : AuthServiceTesting.cs et HashTesting.cs et Installation du nugget Nsubstitute
    Correction de EvenementRequestDto.cs

---
+++ EventAssos.Tests/HashTesting.cs
@@ -0,0 +1,22 @@
+﻿using EventAssos.Infrastructure.Security; 
+using Xunit;
+using Xunit.Abstractions; 
+
+namespace EventAssos.Tests;
+
+public class HashTesting(ITestOutputHelper output)
+{
+  [Fact]
+  public void GenerateAdminHash()
+  {
+    
+    var passwordHasher = new PasswordHasher();
+    string passwordToHash = "Test1234@";
+    
+    string hash = passwordHasher.Hash(passwordToHash);
+    
+    output.WriteLine($"Hash : {hash}");
+        
+    Assert.NotEmpty(hash);
+  }
+}
