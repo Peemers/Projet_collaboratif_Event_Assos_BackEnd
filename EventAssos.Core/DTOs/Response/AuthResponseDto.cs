447bb091112d8b728d34b9780bad8f97bdb149ef
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-07 11:09:00 +0200

    Correction bug AuthResponseDto.cs

--- EventAssos.Core/DTOs/Response/AuthResponseDto.cs
+++ EventAssos.Core/DTOs/Response/AuthResponseDto.cs
@@ -1,7 +1,14 @@
-﻿namespace EventAssos.Core.DTOs.Response;
+﻿using EventAssos.Domain.Enums;
+
+namespace EventAssos.Core.DTOs.Response;
 
 public class AuthResponseDto
 {
   public required string Token { get; init; }
   public required string Pseudo { get; init; }
+  
+  //public required Guid Id { get; init; } Proposition de modification pour pouvoir atteindre le role et l'id facilement
+  
+  //public required Roles Role {get; init; }
+  
 }
