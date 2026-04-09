27b1ef8b48b3de00a9a8f1db2aae9fdb83d27f1f
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-09 09:16:34 +0200

    AuthController.cs : reset password IAuthService.cs Program.cs : injection mailing ResetPasswordDto.cs : A completer

---
+++ EventAssos.Core/DTOs/Request/MembreRequestDtos/ResetPasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventAssos.Core.DTOs.Request.MembreRequestDtos;
+
+public class ResetPasswordDto
+{
+  [Required (ErrorMessage = "Un nouveau password est requis pour le changement")]
+  [RegularExpression(@"^(?=.*[A-Z])(?=.*[!@#$%^&*(),.? "" :{}|<>+=\-])(?=.{6,16}$).*$", ErrorMessage = 
+    "Format de mot de passe invalide (Une majuscule minimum et un caractere spécial minimum")]
+  [MinLength(6, ErrorMessage = "Minimum 6 Caracteres")]
+  [MaxLength(16, ErrorMessage = "Maximum 16 Caracteres")]
+  [DataType(DataType.Password)]
+  public required string Password { get; set; }
+}
--- EventAssos.Core/Interfaces/Services/IAuthService.cs
+++ EventAssos.Core/Interfaces/Services/IAuthService.cs
@@ -8,4 +8,6 @@ public interface IAuthService
   Task<AuthResponseDto> LoginAsync(LoginRequestDto loginDto);
   
   Task<AuthResponseDto> RegisterAsync(RegisterRequestDto registerDto);
+  
+  Task ResetPasswordAsync(string email);
 }
